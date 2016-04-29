5ab3a8aa8ae1f8a72ab5e92caff9e9969a98e8f4
tHEECO <ECO@ECOdeMacBook-Air.local>
2016-04-29 17:31:17 +0800

    commit
    first commit

---
+++ ECO/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECO
+//
+//  Created by ECO on 16/4/29.
+//  Copyright © 2016年 ECO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ECO/Controller/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ECO
+//
+//  Created by ECO on 16/4/29.
+//  Copyright © 2016年 ECO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
