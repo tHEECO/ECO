3c5a7ae029c2f3602c59717cf76119830eec013f
tHEECO <ECO@ECOdeMacBook-Air.local>
2016-04-29 17:38:33 +0800

    创建UIView基类
    创建所有UIView父类 便于统一风格

---
+++ ECO/Base/ECOBaseView.h
@@ -0,0 +1,13 @@
+//
+//  ECOBaseView.h
+//  ECO
+//
+//  Created by ECO on 16/4/29.
+//  Copyright © 2016年 ECO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECOBaseView : UIView
+
+@end
---
+++ ECO/Base/ECOBaseView.m
@@ -0,0 +1,21 @@
+//
+//  ECOBaseView.m
+//  ECO
+//
+//  Created by ECO on 16/4/29.
+//  Copyright © 2016年 ECO. All rights reserved.
+//
+
+#import "ECOBaseView.h"
+
+@implementation ECOBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
